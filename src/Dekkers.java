89b78bc944e9f5d2aa7eca088055015221e2c781
Lars Nysom <LANY@ucn.dk>
2021-02-19 16:54:41 +0100

    Bug fixing

--- src/Dekkers.java
+++ src/Dekkers.java
@@ -4,7 +4,7 @@ public class Dekkers {
 	public static void main(String[] args) {
 		
 		IBuffer buffer = new Buffer();
-		IMutex mutex = new Mutex1();
+		IMutex mutex = new Mutex2();
 		
 		Producer prod = new Producer(buffer, mutex);
 		Consumer cons = new Consumer(buffer, mutex);
